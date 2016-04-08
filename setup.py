c4bd9d07f2469a4d8a0b33e25499dcd7109d342b
diego <daliaga86@gmail.com>
2016-04-08 11:15:13 -0400

    2016-04-08_11-15-08_

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bokeh_plot_database',
-    version='2016-04-05_10-51-02_',
+    version='2016-04-08_11-14-55_',
     packages=['bokeh_plot_database'],
     url='',
     license='',
